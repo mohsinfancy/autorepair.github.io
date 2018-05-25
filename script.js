b8d556b2b77534ee705ecf3a79538d7aed4e9303
Mohsin Maredia <info@mohsinfancy.com>
2018-05-25 00:14:39 -0600

    Update

---
+++ script.js
@@ -0,0 +1,79 @@
+	/*var first = prompt("enter first number");
+	var second = prompt("enter second number");
+	var sum = Number(first) + Number(second);
+	alert(Number(first) + Number(second));
+
+	if (sum > 0 && sum <= 99) {
+		console.log("The Sum is greater than 0"); 
+	} else if (sum >= 100 ) {
+		console.log("The Sum is greater than 100");
+	} else {
+		console.log("the Sum is 0");
+	}*/
+
+
+/*	function sayHello() {
+		console.log("Hello");
+	}
+	
+	var sayBye = function() {
+		console.log("Bye");
+	}
+	
+	sayHello();
+	sayBye();*/
+
+	
+	/*function sing(song) {
+		console.log(song);
+	}
+
+	sing("Munna Bhai MBBS");*/
+
+
+/*	function multiply(a,b) {
+		if(a > 10 || b > 10) {
+			return "thats to hard to calculate :)";
+		} else {
+			return a * b;
+		}
+	}*/
+
+
+
+
+/*function checkDriverAge(age) {
+	var age = prompt("What is your age?");
+
+	if (Number(age) < 18) { 
+		alert("Sorry, you are too yound to drive this car. Powering off"); 
+	} else if (Number(age) > 18) { 
+		alert("Powering On. Enjoy the ride!"); } 
+	else if (Number(age) === 18) { 
+		alert("Congratulations on your first year of driving. Enjoy the ride!"); 
+	}
+
+}
+
+checkDriverAge();
+
+var checkDriverAge2 = function() {
+	var age = prompt("What is your age?");
+
+	if (Number(age) < 18) { 
+		alert("Sorry, you are too yound to drive this car. Powering off"); 
+	} else if (Number(age) > 18) { 
+		alert("Powering On. Enjoy the ride!"); } 
+	else if (Number(age) === 18) { 
+		alert("Congratulations on your first year of driving. Enjoy the ride!"); 
+	}
+
+}
+
+checkDriverAge2();*/
+
+
+
+var nameList = [ ["Mohsin" , "Maqsood" , "Shahid", "Anis", "Sameer" , "Aziz"],
+				["Maredia", "Maredia", "Maredia", "Maredia", "Maredia", "Maredia",]];
+console.log(nameList[0]);
